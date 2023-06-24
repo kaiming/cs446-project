de559d86845295f6877d48fdc3d0204d2e388a56
Jameson Smith <j225smit@uwaterloo.ca>
2023-06-24 16:51:55 -0400

    adding new navigation bar

--- TravelBook/app/src/main/java/com/example/travelbook/navigation/models/NavigationItem.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/navigation/models/NavigationItem.kt
@@ -1,16 +1,14 @@
 package com.example.travelbook.navigation.models
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.travelbook.R
 
 sealed class NavigationItem(
     val name: String,
-    val icon: ImageVector,
+    val icon: Int,
     val route: String
 ) {
-    object Map : NavigationItem("Map", Icons.Filled.Home,"map")
+    object Map : NavigationItem("Map", R.drawable.baseline_map_24,"map")
+    object Trip : NavigationItem("Trip", R.drawable.baseline_airplanemode_active_24, "trip")
+    object Profile : NavigationItem("Profile", R.drawable.baseline_account_circle_24, "profile")
     // TODO: Add other nav paths here
 }
--- TravelBook/app/src/main/java/com/example/travelbook/navigation/views/NavigationView.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/navigation/views/NavigationView.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
@@ -24,6 +26,8 @@ import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -31,6 +35,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -49,24 +56,23 @@ fun NavigationView(
     modifier: Modifier = Modifier
 ) {
     val navigationItems = listOf(
-        NavigationItem.Map
+        NavigationItem.Map,
+        NavigationItem.Trip,
+        NavigationItem.Profile
     )
     val navController = rememberNavController()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
     val selectedItem = remember { mutableStateOf(navigationItems[0]) }
 
-    ModalNavigationDrawer(
-        drawerState = drawerState,
-        gesturesEnabled = drawerState.isOpen,
-        drawerContent = {
-            ModalDrawerSheet {
-                Spacer(Modifier.height(Padding.PaddingLarge.size))
+    Scaffold(
+        bottomBar = {
+            BottomNavigation(
+                backgroundColor = MaterialTheme.colorScheme.background
+            ) {
                 navigationItems.forEach { item ->
-                    NavigationDrawerItem(
+                    BottomNavigationItem(
                         icon = {
                             Icon(
-                                item.icon,
+                                ImageVector.vectorResource(id = item.icon),
                                 contentDescription = null,
                                 tint = MaterialTheme.colorScheme.primary
                             )
@@ -74,7 +80,6 @@ fun NavigationView(
                         label = { Text(item.name) },
                         selected = item == selectedItem.value,
                         onClick = {
-                            scope.launch { drawerState.close() }
                             selectedItem.value = item
                             navController.navigate(item.route)
                         },
@@ -82,49 +87,34 @@ fun NavigationView(
                     )
                 }
             }
-        }
+        },
+        // TODO: Add top bar back in if needed
+//        topBar = {
+//            CenterAlignedTopAppBar(
+//                title = {
+//                    Row(
+//                        horizontalArrangement = Arrangement.SpaceBetween,
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
+//                        Text(
+//                            text = UIText.ResourceString(R.string.app_name).getString(),
+//                            style = MaterialTheme.typography.titleLarge,
+//                            color = MaterialTheme.colorScheme.primary
+//                        )
+//                    }
+//                },
+//                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+//                    containerColor = Color.Transparent
+//                )
+//            )
+//        }
     ) {
-        Scaffold(
-            topBar = {
-                CenterAlignedTopAppBar(
-                    title = {
-                        Row(
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text(
-                                text = UIText.ResourceString(R.string.app_name).getString(),
-                                style = MaterialTheme.typography.titleLarge,
-                                color = MaterialTheme.colorScheme.primary
-                            )
-                        }
-                    },
-                    navigationIcon = {
-                        IconButton(
-                            onClick = {
-                                scope.launch { drawerState.open() }
-                            },
-                            modifier = Modifier
-                                .padding(Padding.PaddingSmall.size)
-                        ) {
-                            Icon(
-                                Icons.Default.Menu,
-                                tint = MaterialTheme.colorScheme.primary,
-                                contentDescription = null,
-                                modifier = Modifier.size(64.dp)
-                            )
-                        }
-                    }
-                )
-            }
-        ) {
-            NavigationGraph(
-                navController = navController,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it)
-            )
-        }
+        NavigationGraph(
+            navController = navController,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        )
     }
 }
 
@@ -135,6 +125,16 @@ fun NavigationGraph(
 ) {
     NavHost(navController, startDestination = NavigationItem.Map.route) {
         composable(NavigationItem.Map.route) {
+            MapView(
+                modifier = modifier
+            )
+        }
+        composable(NavigationItem.Trip.route) {
+            MapView(
+                modifier = modifier
+            )
+        }
+        composable(NavigationItem.Profile.route) {
             MapView(
                 modifier = modifier
             )
