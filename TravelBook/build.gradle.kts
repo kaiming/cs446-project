dea78b4139f30fcefc313b6c6c03944bc7eaa76b
Toluwalogo Jibodu <tjibodu@uwaterloo.ca>
2023-06-24 19:44:21 -0400

    event model and repository created

--- TravelBook/build.gradle.kts
+++ TravelBook/build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id("com.google.gms.google-services") version "4.3.10"
 }
