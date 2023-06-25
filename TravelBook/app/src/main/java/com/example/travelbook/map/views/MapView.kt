ecfa15d37877d0bfead430199f1c8ff8373d1c28
jameson401 <jameson.smith123@gmail.com>
2023-06-25 18:37:13 -0400

    fixing zoom button on map

--- TravelBook/app/src/main/java/com/example/travelbook/map/views/MapView.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/map/views/MapView.kt
@@ -1,15 +1,32 @@
 package com.example.travelbook.map.views
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
 
 @Composable
 fun MapView(
     // TODO: Set up viewmodel and model for the map view
     modifier: Modifier = Modifier
 ) {
-    GoogleMap {
+    val uiSettings = remember { mutableStateOf(
+        MapUiSettings(
+            compassEnabled = true,
+            zoomControlsEnabled = false
+        )
+    ) }
 
+    Box(
+        modifier = modifier
+    ) {
+        GoogleMap(
+            uiSettings = uiSettings.value
+        ) {
+
+        }
     }
 }
