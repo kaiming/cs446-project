ddfde54a8f40266822e3207bafc102a2d939542e
Alex He <alexhe123@outlook.com>
2023-06-25 13:54:42 -0400

    Add missing eventID

--- TravelBook/app/src/main/java/com/example/travelbook/trips/models/EventItem.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/trips/models/EventItem.kt
@@ -2,12 +2,14 @@ package com.example.travelbook.trips.models
 
 
 data class EventItem(
+    @DocumentId
+    var eventId: String,
     var name: String,
     var Date: String,
     var startTime: String,
     var location: String
 ) {
-    constructor() : this("", "", "", "")
+    constructor() : this("", "", "", "", "")
 }
 
 data class EventResponse(
