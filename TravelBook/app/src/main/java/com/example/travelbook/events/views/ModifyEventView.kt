d77615bc5dbadfb33d5ba883b5ee10488cfb7981
CoalGravel <cole.gravelle@gmail.com>
2023-07-18 10:30:46 -0400

    debugging

--- TravelBook/app/src/main/java/com/example/travelbook/events/views/ModifyEventView.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/events/views/ModifyEventView.kt
@@ -34,16 +34,6 @@ fun ModifyEventView(
     onNavigateToEvents: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    if (eventId == null) return
-    val event = viewModel.getEventById(eventId).collectAsState(null).value ?: return
-
-    var eventName by remember { mutableStateOf(TextFieldValue(event.name)) }
-    var eventLocation by remember { mutableStateOf(TextFieldValue(event.location)) }
-    var eventDate by remember { mutableStateOf(TextFieldValue(event.date)) }
-    var eventStartTime by remember { mutableStateOf(TextFieldValue(event.startTime)) }
-    var eventEndTime by remember { mutableStateOf(TextFieldValue(event.endTime)) }
-    var eventCost by remember { mutableStateOf(TextFieldValue(event.cost)) }
-
     Box(
         modifier = modifier
     ) {
@@ -52,98 +42,124 @@ fun ModifyEventView(
             modifier = Modifier.fillMaxSize()
         ) {
             Text(
-                text = "Modify Event",
+                text = eventId ?: "null",
                 fontStyle = MaterialTheme.typography.titleLarge.fontStyle,
                 fontSize = 32.sp,
                 modifier = Modifier.padding(Padding.PaddingSmall.size)
             )
-            TextField(
-                value = eventName,
-                onValueChange = {
-                    eventName = it
-                },
-                label = { Text(text = "Event Name") },
-                placeholder = { Text(text = "Name of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            TextField(
-                value = eventLocation,
-                onValueChange = {
-                    eventLocation = it
-                },
-                label = { Text(text = "Event Location") },
-                placeholder = { Text(text = "Location of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            TextField(
-                value = eventDate,
-                onValueChange = {
-                    eventDate = it
-                },
-                label = { Text(text = "Event Date") },
-                placeholder = { Text(text = "Date of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            TextField(
-                value = eventStartTime,
-                onValueChange = {
-                    eventStartTime = it
-                },
-                label = { Text(text = "Event Start Time") },
-                placeholder = { Text(text = "Start Time of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            TextField(
-                value = eventEndTime,
-                onValueChange = {
-                    eventEndTime = it
-                },
-                label = { Text(text = "Event End Time") },
-                placeholder = { Text(text = "End Time of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            TextField(
-                value = eventCost,
-                onValueChange = {
-                    eventCost = it
-                },
-                label = { Text(text = "Event Cost") },
-                placeholder = { Text(text = "Cost of the Event") },
-                shape = RoundedCornerShape(10.dp),
-                modifier = Modifier.padding(Padding.PaddingSmall.size)
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Button(
-                onClick = {
-                    viewModel.modifyEventItem(
-                        EventItem(
-                            event.eventId,
-                            event.tripId,
-                            eventName.text,
-                            eventDate.text,
-                            eventStartTime.text,
-                            eventEndTime.text,
-                            eventLocation.text,
-                            eventCost.text
-                        )
-                    )
-                    onNavigateToEvents()
-                },
-                modifier = Modifier.padding(Padding.PaddingMedium.size)
-            ) {
-                Text(
-                    text = "Save",
-                    fontStyle = MaterialTheme.typography.titleLarge.fontStyle,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(Padding.PaddingSmall.size)
-                )
-            }
         }
     }
+
+//    if (eventId == null) return
+//    val event = viewModel.getEventById(eventId).collectAsState(null).value ?: return
+//
+//    var eventName by remember { mutableStateOf(TextFieldValue(event.name)) }
+//    var eventLocation by remember { mutableStateOf(TextFieldValue(event.location)) }
+//    var eventDate by remember { mutableStateOf(TextFieldValue(event.date)) }
+//    var eventStartTime by remember { mutableStateOf(TextFieldValue(event.startTime)) }
+//    var eventEndTime by remember { mutableStateOf(TextFieldValue(event.endTime)) }
+//    var eventCost by remember { mutableStateOf(TextFieldValue(event.cost)) }
+//
+//    Box(
+//        modifier = modifier
+//    ) {
+//        Column(
+//            horizontalAlignment = Alignment.CenterHorizontally,
+//            modifier = Modifier.fillMaxSize()
+//        ) {
+//            Text(
+//                text = "Modify Event",
+//                fontStyle = MaterialTheme.typography.titleLarge.fontStyle,
+//                fontSize = 32.sp,
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventName,
+//                onValueChange = {
+//                    eventName = it
+//                },
+//                label = { Text(text = "Event Name") },
+//                placeholder = { Text(text = "Name of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventLocation,
+//                onValueChange = {
+//                    eventLocation = it
+//                },
+//                label = { Text(text = "Event Location") },
+//                placeholder = { Text(text = "Location of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventDate,
+//                onValueChange = {
+//                    eventDate = it
+//                },
+//                label = { Text(text = "Event Date") },
+//                placeholder = { Text(text = "Date of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventStartTime,
+//                onValueChange = {
+//                    eventStartTime = it
+//                },
+//                label = { Text(text = "Event Start Time") },
+//                placeholder = { Text(text = "Start Time of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventEndTime,
+//                onValueChange = {
+//                    eventEndTime = it
+//                },
+//                label = { Text(text = "Event End Time") },
+//                placeholder = { Text(text = "End Time of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            TextField(
+//                value = eventCost,
+//                onValueChange = {
+//                    eventCost = it
+//                },
+//                label = { Text(text = "Event Cost") },
+//                placeholder = { Text(text = "Cost of the Event") },
+//                shape = RoundedCornerShape(10.dp),
+//                modifier = Modifier.padding(Padding.PaddingSmall.size)
+//            )
+//            Spacer(modifier = Modifier.weight(1f))
+//            Button(
+//                onClick = {
+//                    viewModel.modifyEventItem(
+//                        EventItem(
+//                            event.eventId,
+//                            event.tripId,
+//                            eventName.text,
+//                            eventDate.text,
+//                            eventStartTime.text,
+//                            eventEndTime.text,
+//                            eventLocation.text,
+//                            eventCost.text
+//                        )
+//                    )
+//                    onNavigateToEvents()
+//                },
+//                modifier = Modifier.padding(Padding.PaddingMedium.size)
+//            ) {
+//                Text(
+//                    text = "Save",
+//                    fontStyle = MaterialTheme.typography.titleLarge.fontStyle,
+//                    fontSize = 24.sp,
+//                    modifier = Modifier.padding(Padding.PaddingSmall.size)
+//                )
+//            }
+//        }
+//    }
 }
 
