195eff471275006516b5d482adddc6cde552185a
Kaiming Qiu <kaiming.qiu5@gmail.com>
2023-07-19 22:09:29 -0400

    added budget progress bar to itinerary view

--- TravelBook/app/src/main/java/com/example/travelbook/events/views/EventView.kt
+++ TravelBook/app/src/main/java/com/example/travelbook/events/views/EventView.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -17,10 +20,12 @@ import androidx.compose.material.icons.rounded.AddCircle
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -40,11 +45,18 @@ fun EventView(
     if (tripId !is String) return
 
     val events = viewModel.getEventsFlowByTripId(tripId).collectAsStateWithLifecycle(initialValue = emptyList())
+
+    var totalCosts = 0f
+    for (event in events.value) {
+        totalCosts += event.cost.toFloat()
+    }
+
     Box(modifier = modifier) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
+            BudgetProgressBar(currentBudget = totalCosts, totalBudget = 1000f)
             Text(
                 text = "Itinerary",
                 fontStyle = MaterialTheme.typography.titleLarge.fontStyle,
@@ -94,7 +106,8 @@ private fun EventCard(
 ) {
     Card(
         shape = RoundedCornerShape(15.dp),
-        modifier = Modifier.padding(Padding.PaddingMedium.size)
+        modifier = Modifier
+            .padding(Padding.PaddingMedium.size)
             .clickable { onClick() }
     ) {
         Row(modifier = Modifier.padding(Padding.PaddingExtraLarge.size)) {
@@ -119,5 +132,27 @@ private fun EventCard(
                 // space for image/event logo... don't think the model supports this yet.
             }
         }
+    }
+}
+
+@Composable
+fun BudgetProgressBar(currentBudget: Float, totalBudget: Float) {
+    // Calculate the progress percentage
+    val progress = (currentBudget / totalBudget).coerceIn(0f, 1f)
+
+    Box(modifier = Modifier.fillMaxWidth()) {
+        // LinearProgressIndicator to display the progress
+        LinearProgressIndicator(
+            progress = progress,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        // Spacer to fill the remaining space
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(4.dp) // Adjust the height of the progress bar
+                .background(Color.Transparent)
+        )
     }
 }
